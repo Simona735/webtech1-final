bc7ac3aa9e1a447a4e4ce32449fc985d1fb63f23
Simona735 <72383935+Simona735@users.noreply.github.com>
2020-12-27 18:06:13 +0100

    slight changes in design

--- resources/components/NamesdayWidget.js
+++ resources/components/NamesdayWidget.js
@@ -1,15 +1,32 @@
 const template = document.createElement('template');
 template.innerHTML = `
-        <div class="row">
-            <h4 class="mt-2">Zmena amplitúdy</h4>
-        </div>
-            <style>
-                #sliderInput, #numberInput, #sliderV{
-                    display: none;
-                }
-            </style>
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-        </div>
+        <aside class="col-md-2 mt-3">
+                <div class="card">
+                    <article class="card-group-item">
+                        <header class="card-header">
+                            <h6 class="title">Widget 1</h6>
+                        </header>
+                        <div class="filter-content">
+                            <div class="card-body">
+                                <div class="custom-control custom-checkbox">
+                                    <span class="float-right badge badge-light round">52</span>
+                                    <input type="checkbox" class="custom-control-input" id="Check1">
+                                    <label class="custom-control-label" for="Check1">Samsung</label>
+                                </div> <!-- form-check.// -->
+
+                                <div class="custom-control custom-checkbox">
+                                    <span class="float-right badge badge-light round">132</span>
+                                    <input type="checkbox" class="custom-control-input" id="Check2">
+                                    <label class="custom-control-label" for="Check2">Black berry</label>
+                                </div> <!-- form-check.// -->
+                            </div> <!-- card-body.// -->
+                        </div>
+                    </article> <!-- card-group-item.// -->
+                </div> <!-- card.// -->
+            </aside>
+        <style>
+        </style>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
         `;
 
 class NamesdayWidget extends HTMLElement {
